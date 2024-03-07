886af11d700feac215c3f25fef592886535e5159
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-07 16:36:38 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package jk1
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+      // 呼び出し
+    jk1.Person person1 = jk1.new Person("鈴木太郎", 20, 1.7,60);
+    // 使用時
+    System.out.println(person1.name);
+    System.out.println(person1.age);
+    System.out.println(person1.height);
+    System.out.println("");
+    person1.bmi();
+    person1.print();
+    
+ System.out.println("合計人数は"+Person.s+"人です。");
+  }
+}
