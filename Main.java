1584429c524a0b12ff1f7520bf3e922222a541ba
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-07 21:44:22 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
       // 呼び出し
-    jk1.Person person1 = jk1.new Person("鈴木太郎", 20, 1.7,60);
+    jk1.Person person1 = new jk1.Person("鈴木太郎", 20, 1.7,60);
     // 使用時
     System.out.println(person1.name);
     System.out.println(person1.age);
